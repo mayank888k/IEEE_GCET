cab46a99a6cafc4c9c2dfad59d846c5bc55cd1ca
Mayank Kumar <mayank963k@gmail.com>
2022-11-30 01:46:25 +0530

    Speakers Layout Update

--- src/components/speakerSection/speaker.jsx
+++ src/components/speakerSection/speaker.jsx
@@ -16,164 +16,246 @@ import img13 from './speakerImgs/DrSubharnaShakya.jpeg';
 import img14 from './speakerImgs/ahmed.jpg';
 import img15 from './speakerImgs/kashif.jpg';
 import img16 from './speakerImgs/anaClark.jpg';
+import img17 from './speakerImgs/Nada.jpg';
 
 
 const Speakers = () => {
     document.title = "Speakers | ICAC3N - 4th IEEE International Conference on Advances in Computing, Communication Control and Networking";
   return (
     <div className="speaker">
+        <h1 style={{textAlign:"center"}} className="heading">Speakers</h1>
+
+        <div className="top_box">
+            
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img1} alt="img1"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Sri Niwas Singh </strong> 
+                            <br/> 
+                            Director, Atal Bihari Vajpayee- Indian Institute of Information Technology and Management (ABV-IIITM), Gwalior, India / Chairman, India Council, IEEE India.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img12} alt="img12"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) J Ramkumar </strong> 
+                            <br/> 
+                            Professor, IIT Kanpur India, Ex Chair of IEEE UP
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img2} alt="img2"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Rajiv Saxena </strong> 
+                            <br/> 
+                            Vice Chancellor, Jaypee University, Anoopshahr.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img14} alt="img14"/>
+                        <figcaption> 
+                            <strong> Dr. Ahmed A. Elngar </strong> 
+                            <br/> 
+                            Associate Professor, Faculty of CS and AI, Beni-Suef University, 
+                            Beni-Suef City, 62511, Egypt
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img17} alt="img17"/>
+                        <figcaption> 
+                            <strong>Prof. Nada Ratković, Croatia </strong> 
+                            <br/> 
+                            Assistant Professor, Department of Quantitative Methods on Faculty of Economics, 
+                            Business and Tourism, University Split.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img15} alt="img15"/>
+                        <figcaption> 
+                            <strong> Dr. Kashif Nisar, SMIEEE </strong> 
+                            <br/> 
+                            Victorian Institute of Technology,
+                            14 Adam Street, Hindmarsh, Adelaide, South Australia, 5007
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img16} alt="img16"/>
+                        <figcaption> 
+                            <strong> Dr. Ana Clarke </strong> 
+                            <br/> 
+                            Partner Director – Data & AI , Artificial Intelligence Services Australia.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img3} alt="img3"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Deepak Garg </strong> 
+                            <br/> 
+                            Director leadingindia.ai and Head
+                            CSE, Bennett University and Director, 
+                            NVIDIA-Bennett Centre of Research on AI.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img4} alt="img4"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Siddhaling Urolagin </strong> 
+                            <br/> 
+                            Department of Computer Science, Birla Institute of Technology & Science, Pilani, Dubai International Academic City.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img5} alt="img5"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Celestine Iwendi </strong> 
+                            <br/> 
+                            School of Creative Technologies, University of Bolton, United Kingdom / Board Member IEEE Sweden Section.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img13} alt="img13"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Subarna Shakya </strong> 
+                            <br/> 
+                            Professor, Department of Electronics and Computer Engineering, Tribhuvan University, Nepal.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img6} alt="img6"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Satish K. Singh </strong> 
+                            <br/> 
+                            IIIT Allahabad / Section Chair, IEEE UP Section.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img7} alt="img7"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Asheesh K. Singh </strong> 
+                            <br/> 
+                            Professor EE Department, MNNIT, Allahabad / Immediate Past Section Chair, IEEE UP Section.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img8} alt="img8"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Prabhakar Tiwari </strong> 
+                            <br/> 
+                            MMMUT, Gorakhpur/ Secretary, IEEE U.P. Section.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img9} alt="img9"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Malay Kishore Dutta </strong> 
+                            <br/> 
+                            Director, Dean PG studies and Research, Dr. A.P.J. Abdul Kalam Technical University, Lucknow.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img10} alt="img10"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) Neetesh Purohit </strong> 
+                            <br/> 
+                            Indian Institute of Information Technology, Allahabad (IIITA), Prayagraj, UP.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+            <div className="outer_box">
+                <div className="inner_box">
+                    <figure>
+                        <img style={{height:"200px", width:"180px", marginBottom:"15px"}} src={img11} alt="img11"/>
+                        <figcaption> 
+                            <strong> Prof. (Dr.) N. Badal </strong> 
+                            <br/> 
+                            Director, REC, Bijnor (U.P.), India.
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+        </div>
+
         <div className="matter">
-            <h1 className="heading">Speakers</h1>
-
-            <figure>
-                <img src={img1} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Sri Niwas Singh </strong> 
-                    <br/> 
-                    Director, Atal Bihari Vajpayee- Indian Institute of Information Technology and Management (ABV-IIITM), Gwalior, India / Chairman, India Council, IEEE India.
-                </figcaption>
-            </figure>
-
-            <figure>
-                <img style={{height:"200px", width:"180px"}} src={img12} alt="img12"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) J Ramkumar </strong> 
-                    <br/> 
-                    Professor, IIT Kanpur India, Ex Chair of IEEE UP
-                </figcaption>
-            </figure>
-
-            <figure>
-                <img src={img2} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Rajiv Saxena </strong> 
-                    <br/> 
-                    Vice Chancellor, Jaypee University, Anoopshahr.
-                </figcaption>
-            </figure>
-
-            <figure>
-                <img style={{height:"200px", width:"180px"}} src={img14} alt="img1"/>
-                <figcaption> 
-                    <strong> Dr. Ahmed A. Elngar </strong> 
-                    <br/> 
-                    
-                    Associate Professor, Faculty of CS and AI, Beni-Suef University, 
-                    Beni-Suef City, 62511, Egypt
-
-                </figcaption>
-            </figure>
-
-            <figure>
-                <img style={{height:"200px", width:"180px"}} src={img15} alt="img1"/>
-                <figcaption> 
-                    <strong> Dr. Kashif Nisar, SMIEEE </strong> 
-                    <br/> 
-                    Victorian Institute of Technology,
-                    14 Adam Street, Hindmarsh, Adelaide, South Australia, 5007
-                </figcaption>
-            </figure>
-
-            <figure>
-                <img style={{height:"200px", width:"180px"}} src={img16} alt="img1"/>
-                <figcaption> 
-                    <strong> Dr. Ana Clarke(PhD) </strong> 
-                    <br/> 
-                    Partner Director – Data & AI , Artificial Intelligence Services Australia.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img3} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Deepak Garg </strong> 
-                    <br/> 
-                    Director leadingindia.ai and Head
-                    CSE, Bennett University and Director, 
-                    NVIDIA-Bennett Centre of Research on AI.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img4} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Siddhaling Urolagin </strong> 
-                    <br/> 
-                    Department of Computer Science, Birla Institute of Technology & Science, Pilani, Dubai International Academic City.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img5} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Celestine Iwendi </strong> 
-                    <br/> 
-                    School of Creative Technologies, University of Bolton, United Kingdom / Board Member IEEE Sweden Section.
-                </figcaption>
-            </figure>
-
-            <figure>
-                <img style={{height:"220px", width:"200px"}} src={img13} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Subarna Shakya </strong> 
-                    <br/> 
-                    Professor, Department of Electronics and Computer Engineering, Tribhuvan University, Nepal.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img6} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Satish K. Singh </strong> 
-                    <br/> 
-                    IIIT Allahabad / Section Chair, IEEE UP Section.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img7} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Asheesh K. Singh </strong> 
-                    <br/> 
-                    Professor EE Department, MNNIT, Allahabad / Immediate Past Section Chair, IEEE UP Section.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img8} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Prabhakar Tiwari </strong> 
-                    <br/> 
-                    MMMUT, Gorakhpur/ Secretary, IEEE U.P. Section.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img9} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Malay Kishore Dutta </strong> 
-                    <br/> 
-                    Director, Dean PG studies and Research, Dr. A.P.J. Abdul Kalam Technical University, Lucknow.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img10} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) Neetesh Purohit </strong> 
-                    <br/> 
-                    Indian Institute of Information Technology, Allahabad (IIITA), Prayagraj, UP.
-                </figcaption>
-            </figure>
-            
-            <figure>
-                <img src={img11} alt="img1"/>
-                <figcaption> 
-                    <strong> Prof. (Dr.) N. Badal </strong> 
-                    <br/> 
-                    Director, REC, Bijnor (U.P.), India.
-                </figcaption>
-            </figure>
 
         </div>
     </div>
